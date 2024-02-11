eabf234a2ad5e7ecda05e7fc07fb836630ba02d9
shubham kumar <shubhofficial1@gmail.com>
2024-02-12 01:39:49 +0530

    PreRendering concept in nextjs

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,6 @@
 export default function page() {
+  // Notice how in dev mode , the console.log apprear in vscode console instead of browser.
+  console.log("[HomePage] rendering");
   return (
     <>
       <h1>Indie Gamer</h1>
